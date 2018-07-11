cf57fa41cf28bf1315ee6ccced969e9134385778
imisinn <shinnya19971027@gmail.com>
2018-07-11 22:04:06 +0900

    最大最小の入力欄追加、入力欄エラー検出追加

--- sample5.php
+++ sample5.php
@@ -8,7 +8,10 @@
 <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
    <p>
    検索キーワード：<input type="text" name="keyword" size=20/><br>
+   <br>人数範囲指定か人数指定どちらかにしてください<br><br>
    写真中の人の数：<input type="text" name="number" size=20 /><br>
+   写真中の人の最大の数：<input type="text" name="number_max" size=20 /><br>
+   写真中の人の最小の数：<input type="text" name="number_min" size=20 /><br>
    <input type="submit" value="Search!" />
    </p>
 </form>
@@ -45,18 +48,34 @@ fclose($f2);
 
 // 以下、検索処理
 $result_num = 0;
+$flag = 0;
 
-if (isset($_POST["keyword"]) && isset($_POST["number"])) {
+if (isset($_POST["keyword"]) && isset($_POST["number"]) && isset($_POST["number_max"]) && isset($_POST["number_min"])) {
   if(array_key_exists($_POST["keyword"], $tf_data)){
     $nump = mb_convert_kana($_POST["number"], "n", "utf-8");
+    $max = mb_convert_kana($_POST["number_max"], "n", "utf-8");
+    $min = mb_convert_kana($_POST["number_min"], "n", "utf-8");
 
-	if ($_POST["number"]==null || !preg_match("/^[0-9]+$/", $nump)) {
-		echo "人数を正しく入力して下さい。";
-	} else {
+
+//!preg_match("/^[0-9]+$/", $nump) || !preg_match("/^[0-9]+$/", $max)  || !preg_match("/^[0-9]+$/", $min)
+	if($_POST["number"]==null && $_POST["number_max"]==null &&  $_POST["number_min"]==null  ){
+		echo "人数を入力して下さい。";
+        $flag = -1;
+	}else if($_POST["number"]!=null && !preg_match("/^[0-9]+$/", $nump)){
+        echo "人数を正しく入力してください。";
+        $flag = -1;
+    }else if($_POST["number_max"]!=null && !preg_match("/^[0-9]+$/", $max)){
+        echo "人数を正しく入力してください。";
+        $flag = -1;
+    }elseif ($_POST["number_min"]!=null && !preg_match("/^[0-9]+$/", $min)){
+        echo "人数を正しく入力してください。";
+        $flag = -1;
+    } else {
     	echo "キーワード「".$_POST["keyword"]."」　人数「";
     	echo $_POST["number"]."人」での検索結果<br>\n";
     }
     echo "<hr><br>\n";
+    if($flag == -1)echo "検索中止";
 
     foreach($tf_data[@$_POST["keyword"]] as $key => $val ) {
       if ($nump == @$fc_data[$key] && $nump<>null){
