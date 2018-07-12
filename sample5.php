aa962601b036f49e55d69125b890e0bb2e090fae
imisinn <shinnya19971027@gmail.com>
2018-07-12 12:38:50 +0900

    インデント修正

--- sample5.php
+++ sample5.php
@@ -68,72 +68,68 @@ if (isset($_POST["keyword"]) && isset($_POST["number"]) && isset($_POST["number_
       	}else if($_POST["number"]!=null && !preg_match("/^[0-9]+$/", $nump)){
               echo "人数を正しく入力してください。";
               $flag = 3;
-          }else if($_POST["number_max"]!=null && !preg_match("/^[0-9]+$/", $max)){
+        }else if($_POST["number_max"]!=null && !preg_match("/^[0-9]+$/", $max)){
               echo "人数を正しく入力してください。";
               $flag = 3;
-          }elseif ($_POST["number_min"]!=null && !preg_match("/^[0-9]+$/", $min)){
+        }elseif ($_POST["number_min"]!=null && !preg_match("/^[0-9]+$/", $min)){
               echo "人数を正しく入力してください。";
               $flag = 3;
-          }elseif ($nump != null && ($max != null || $min != null)) {
+        }elseif ($nump != null && ($max != null || $min != null)) {
               echo "範囲指定か、単一な人数指定かどちらかにしてください :2";
               $flag = 3;
-          }else {
-          }
-          echo "<hr><br>\n";
-          //echo "flag : '$flag'　<br>";
+        }else {
+        }
+        echo "<hr><br>\n";
 
-          if($flag != 3){//入力エラーミス出ない場合
-              if($max == null && $min != null){//最大が入力されていない場合
-                  //echo "最大未入力<br>";
-                  $flag = 2;
-                  $max = "1000";
-                  echo "キーワード「".$oneword."」　人数「";
-                  echo $min."人以上」での検索結果<br>\n";
-              }elseif ($min == null && $max != null){//最小が入力されていない場合
-                  //echo "最小未入力<br>";
-                  $flag = 2;
-                  $min = "0";
-                  echo "キーワード「".$oneword."」　人数「";
-                  echo $max."人以下」の検索結果<br>\n";
-              }elseif ($nump != null){//単一指定の場合
-                  //echo "単一指定<br>";
-                  $flag = 1;
-                  echo "キーワード「".$oneword."」　人数「";
-                  echo $nump."人」での検索結果<br>\n";
-              }else{//最大、最小が入力されている場合
-                  //echo "範囲両方指定<br>";
-                  $flag = 2;
-                  echo "キーワード「".$oneword."」　人数「";
-                  echo $min."人以上".$max."人以下」での検索結果<br>\n";
-              }
-          }
 
+        if($flag != 3){//入力エラーミス出ない場合
+            if($max == null && $min != null){//最大が入力されていない場合
+                //echo "最大未入力<br>";
+                $flag = 2;
+                $max = "1000";
+                echo "キーワード「".$oneword."」　人数「";
+                echo $min."人以上」での検索結果<br>\n";
+            }elseif ($min == null && $max != null){//最小が入力されていない場合
+                //echo "最小未入力<br>";
+                $flag = 2;
+                $min = "0";
+                echo "キーワード「".$oneword."」　人数「";
+                echo $max."人以下」の検索結果<br>\n";
+            }elseif ($nump != null){//単一指定の場合
+                //echo "単一指定<br>";
+                $flag = 1;
+                echo "キーワード「".$oneword."」　人数「";
+                echo $nump."人」での検索結果<br>\n";
+            }else{//最大、最小が入力されている場合
+                //echo "範囲両方指定<br>";
+                $flag = 2;
+                echo "キーワード「".$oneword."」　人数「";
+                echo $min."人以上".$max."人以下」での検索結果<br>\n";
+            }
+        }
+        if($flag != 3){
+            foreach($tf_data[$oneword] as $key => $val ) {
+                if($flag == 1){
+                    if ($nump == @$fc_data[$key]){
+                      echo "<img src='$key'><br>\n";
+                      echo "キーワード出現回数＝".$val."回<br>\n";
+                      echo "写真中の人の数＝".@$fc_data[$key]."人<br>\n";
+                      echo "$key<br><br><br>\n";
+                      $result_num++;
+                    }
+                }elseif ($flag == 2) {
+                    if ($min <= @$fc_data[$key] && $max >= @$fc_data[$key]){
+                      echo "<img src='$key'><br>\n";
+                      echo "キーワード出現回数＝".$val."回<br>\n";
+                      echo "写真中の人の数＝".@$fc_data[$key]."人<br>\n";
+                      echo "$key<br><br><br>\n";
+                      $result_num++;
+                    }
+                }
+            }
+        }
 
           //echo "<br><br>単一指定 '$nump' 最小：　'$min' 最大: '$max' flag: '$flag'<br><br>";
-
-
-          if($flag != 3){
-              foreach($tf_data[$oneword] as $key => $val ) {
-                  if($flag == 1){
-                      if ($nump == @$fc_data[$key]){
-                		echo "<img src='$key'><br>\n";
-                		echo "キーワード出現回数＝".$val."回<br>\n";
-                		echo "写真中の人の数＝".@$fc_data[$key]."人<br>\n";
-                		echo "$key<br><br><br>\n";
-                		$result_num++;
-                      }
-                  }elseif ($flag == 2) {
-                      if ($min <= @$fc_data[$key] && $max >= @$fc_data[$key]){
-                		echo "<img src='$key'><br>\n";
-                		echo "キーワード出現回数＝".$val."回<br>\n";
-                		echo "写真中の人の数＝".@$fc_data[$key]."人<br>\n";
-                		echo "$key<br><br><br>\n";
-                		$result_num++;
-                      }
-                  }
-              }
-
-          }
 
       } elseif ($oneword==null) {
         echo '検索キーワードを入力して下さい。';
